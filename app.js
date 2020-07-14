baff7c598526a7f42a7c40fc9d06f61c6bf2eec5
maulikindia <60645304+maulikindia@users.noreply.github.com>
2020-07-14 18:46:46 +0530

    app.js changes for reading from .env file

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ let express = require('express');
 
 let app = express();
 
-if (process.env.node_env === 'test') {
-    require('dotenv').config();
-}
+// if (process.env.NODE_ENV === 'test') {
+//     require('dotenv').config();
+// }
+// else if (process.env.NODE_ENV === 'production') {
+//     require('dotenv').config();
+//     console.log('here')
+// }
 
+let config=require('dotenv').config()
 
+console.log(process.env.port);
+console.log(process.env.node_env);
 app.listen(5711, async (err) => {
     if (!err) {
-        console.log('server is running on ' + process.env.PORT);
+        console.log('server is running on ' + process.env.port);
     }
 });
